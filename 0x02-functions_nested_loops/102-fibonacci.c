1fff5e7f3a96afa650c6b9598ebb8632a87e62b6
Alex Allen <325@holbertonschool.com>
2018-01-23 16:52:28 +0000

    one less fib num

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -17,10 +17,10 @@ int main(void)
 	printf("%.0f, ", fib1);
 	printf("%.0f, ", fib2);
 
-	for (i = 2; i < 50; ++i)
+	for (i = 2; i < 49; ++i)
 	{
 		fibCurrent = fib1 + fib2;
-		if (i < 49)
+		if (i < 48)
 			printf("%.0f, ", fibCurrent);
 		else
 			printf("%.0f\n", fibCurrent);
