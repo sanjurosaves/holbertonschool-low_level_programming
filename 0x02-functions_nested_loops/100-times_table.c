f51757018f96db4eb5a4cda251342199279d11a0
Alex Allen <325@holbertonschool.com>
2018-01-24 13:50:12 +0000

    correcting for zero input

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,7 @@ void print_times_table(int n)
 
 		for (i = 0; i < (n + 1); i++)
 		{
-			temp = i; _putchar('0'); _putchar(',');
+			temp = i; _putchar('0'); if (n > 0) _putchar(',');
 			for (j = 0; j < n; j++)
 			{
 				if (j != 0)
