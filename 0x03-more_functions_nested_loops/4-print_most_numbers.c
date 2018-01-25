3689392414c90723e7deb5b2176dd4d531d31506
Alex Allen <325@holbertonschool.com>
2018-01-25 21:59:53 +0000

    more conventionally

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,8 @@ void print_most_numbers(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-		_putchar((i != 2 && i != 4) ? i + '0' : 30);
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
 
 	_putchar('\n');
 }
