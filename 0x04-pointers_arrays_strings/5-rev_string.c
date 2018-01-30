600c5444a7e884a4982518df84dacbe1ad2cf716
Alex Allen <325@holbertonschool.com>
2018-01-30 21:55:45 +0000

    removing 2nd function

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,37 +1,24 @@
 #include "holberton.h"
 
 /**
- * _strlen - counts characters in a string
- * @s: string
- * Return: string length
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-
-	return (i);
-}
-
-/**
  * rev_string - reverses a string
  * @s: string
  * Return: void
  */
 void rev_string(char *s)
 {
-	int i;
+	int h;
 	int j;
 	int k;
 	char tmp[98];
 
-	i = _strlen(s);
-	k = i;
+	for (h = 0; s[h] != '\0'; h++)
+		;
 
-	for (j = 0; i > 0; i--, j++)
-		tmp[j] = s[i - 1];
+	k = h;
+
+	for (j = 0; h > 0; h--, j++)
+		tmp[j] = s[h - 1];
 
 	for (j = 0; k > 0; k--, j++)
 		s[j] = tmp[j];
