770300ff7480752873756956227ac0dc54537e34
Alex Allen <325@holbertonschool.com>
2018-01-31 15:19:58 +0000

    iterate before

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -8,20 +8,10 @@
 void puts2(char *str)
 {
 	int i;
-	int c;
 
-	c = 0;
-
-	for (i = 0; str[i] != '\0'; i++, c++)
-		;
-
-	i = 0;
-
-	while (i <= c)
-	{
-		_putchar(str[i]);
-		i += 2;
-	}
+	for (i = 0; str[i] != '\0'; ++i)
+		if (i % 2 == 0)
+			_putchar(str[i]);
 
 	_putchar('\n');
 
