46a91fc7a7eff6ef23d8fd88ff34a4104a2bde3a
Alex Allen <325@holbertonschool.com>
2018-01-31 15:23:10 +0000

    iterate earlier

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,12 @@ void puts_half(char *str)
 
 	if (i % 2 == 0)
 	{
-		for (i = i / 2; str[i] != '\0'; i++)
+		for (i = i / 2; str[i] != '\0'; ++i)
 		_putchar(str[i]);
 	}
 	else
 	{
-		for (i = (i - 1) / 2; str[i] != '\0'; i++)
+		for (i = (i - 1) / 2; str[i] != '\0'; ++i)
 		_putchar(str[i]);
 	}
 
