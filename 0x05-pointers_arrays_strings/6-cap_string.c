2190508d586071a329832843f74fd67892db5fd3
Alex Allen <325@holbertonschool.com>
2018-02-01 21:36:52 +0000

    account for lower case first char

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -12,10 +12,13 @@ char *cap_string(char *str)
 
 	s = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	if (str[0] >= 97 && str[0] <= 122)
+		str[0] = str[0] - 32;
+
+	for (i = 1; str[i] != '\0'; i++)
 	{
 		if (str[i] == 44 || str[i] == 59 || str[i] == 46
-		    || str[i] == 33 || str[i] == 63 || str[i] ==  34
+		    || str[i] == 33 || str[i] == 63 || str[i] == 34
 		    || str[i] == 40 || str[i] == 41 || str[i] == 123
 		    || str[i] == 125 || str[i] == 32 || str[i] == 9
 		    || str[i] == 10)
