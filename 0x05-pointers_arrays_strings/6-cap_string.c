b63e3c31e71a9159789895747a9763feeb48c35e
Alex Allen <325@holbertonschool.com>
2018-02-01 21:24:55 +0000

    checking by ASCII value

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -14,10 +14,10 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ',' || str[i] == ';' || str[i] == '.'
-		    || str[i] == '!' || str[i] == '?' || str[i] ==  '"'
-		    || str[i] == '(' || str[i] == ')' || str[i] == '{'
-		    || str[i] == '}' || str[i] == ' ' || str[i] == 9
+		if (str[i] == 44 || str[i] == 59 || str[i] == 46
+		    || str[i] == 33 || str[i] == 63 || str[i] ==  34
+		    || str[i] == 40 || str[i] == 41 || str[i] == 123
+		    || str[i] == 125 || str[i] == 32 || str[i] == 9
 		    || str[i] == 10)
 			s = 1;
 
