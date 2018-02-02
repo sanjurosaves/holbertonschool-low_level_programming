ea6562a1f8f9df8a4aa67e322c9f509b61e034b6
Alex Allen <325@holbertonschool.com>
2018-02-02 00:46:00 +0000

    disallow n of zero

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	unsigned short int end = 0;
 
-	for (i = 0; end != 1 && n >= 0; i++)
+	for (i = 0; end != 1 && n > 0; i++)
 	{
 		dest[i] = src[i];
 		if (src[i] == '\0' || src[i + 1] == src[n])
