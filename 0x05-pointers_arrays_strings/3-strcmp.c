6f6fd5e5eb09f8ba1bfbf52935aef6912d1f0d3c
Alex Allen <325@holbertonschool.com>
2018-02-02 23:19:32 +0000

    simpler solution

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+ #include "holberton.h"
 
 /**
  * _strcmp - compares char arrays
@@ -8,16 +8,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	short int i = 0;
+	short int i;
 
-	do {
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		else if (s1[i] == '\0' && s2[i] == '\0')
-			return (0);
+	for (i = 0; s1[i] != '\0' && s1[i] == s2[i]; i++)
+		;
 
-		i++;
-	} while (s1[i] != s2[i]);
-
-	return (0);
+	return (s1[i] - s2[i]);
 }
