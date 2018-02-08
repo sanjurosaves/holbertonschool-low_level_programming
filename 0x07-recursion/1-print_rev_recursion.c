bc5712249454b105c4a9667c695845d5c139e453
Alex Allen <325@holbertonschool.com>
2018-02-08 05:51:28 +0000

    add numb 1

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string to reverse and print
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
