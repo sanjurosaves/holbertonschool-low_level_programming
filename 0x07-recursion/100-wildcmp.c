2f83af13ed36d28ec264caaeaeeff044aa439c56
Alex Allen <325@holbertonschool.com>
2018-02-08 23:01:41 +0000

    add task 8 and updated header file

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares 2 strings accounting for wildcard function
+ * @s2: second string, subject to contain wildcards
+ * @s1: first string
+ * Return: 1 if "identical", 0 if different
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+
+	return (0);
+}
