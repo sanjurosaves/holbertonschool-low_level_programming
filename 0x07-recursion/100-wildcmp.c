3e4a7a565ccb876d74d16f57be5689a4fa102fd3
Alex Allen <325@holbertonschool.com>
2018-02-08 23:52:22 +0000

    accounting for s1 being null and s2 being *

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -14,6 +14,9 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
+	if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+
 	if (*s2 == '*')
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 
