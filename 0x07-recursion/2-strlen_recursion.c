c537983040b9913ebc06143c7c6ab9f452f9e21b
Alex Allen <325@holbertonschool.com>
2018-02-08 01:09:14 +0000

    remove static variable and make betty-compliant

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
+/**
+ * _strlen_recursion - returns the length of string
+ * @s: pointer to string
+ * Return: length
+ */
 int _strlen_recursion(char *s)
 {
-	static int i = 0;
+	int i = 0;
 
-	if (*s != '\0')
-	{
-		i++;
-		_strlen_recursion(++s);
-	}
+	if (s[i] == '\0')
+		return (i);
 
-	return (i);
+	return (1 + (_strlen_recursion(&s[i + 1])));
+
 }
