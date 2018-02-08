6901142da2774f65d71389d3cc593227bd6c64a8
Alex Allen <325@holbertonschool.com>
2018-02-08 05:49:47 +0000

    add tasks 2 and 7

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of string
+ * @s: pointer to string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+		return (i);
+
+	return (1 + (_strlen_recursion(&s[i + 1])));
+}
+
+/**
+ * compare - compares each half of string against each other
+ * @end: last letter in string
+ * @start: first letter in string
+ * @s: pointer to string
+ * Return: 1 if strings match, 1 if string is 1 char, 0 otherwise
+ */
+int compare(char *s, int start, int end)
+{
+	if (start == end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	if (*s < end + 1)
+		return (compare(s, start + 1, end - 1));
+
+	return (1);
+}
+
+/**
+ * is_palindrome - determines if string is a palindrome
+ * @s: pointer to the string
+ * Return: 1 if so; 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int end = _strlen_recursion(s);
+
+	if (end == 0)
+		return (1);
+
+	return (compare(s, 0, end - 1));
+}
