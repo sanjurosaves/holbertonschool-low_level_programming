26fd0160ca3f38620059240f47d11608a10955ec
Alex Allen <325@holbertonschool.com>
2018-02-12 00:45:08 +0000

    correcting comments for betty

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -6,7 +6,7 @@
  * main - prints sum
  * @argv: pointer to array of arguments
  * @argc: count of arguments
- * Return: 0 if successful, 1 if Error of != 2 arguments;
+ * Return: 0 if successful, 1 if a non-numeric char is entered
  */
 int main(int argc, char **argv)
 {
@@ -23,10 +23,10 @@ int main(int argc, char **argv)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 			if (argv[i][j] < 48 || argv[i][j] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
+			{
+				printf("Error\n");
+				return (1);
+			}
 	}
 
 	for (i = 1; i < argc; i++)
