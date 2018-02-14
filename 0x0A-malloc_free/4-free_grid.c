0f99d56257800fc1ece74bc5822088fd6b8dec8e
Alex Allen <325@holbertonschool.com>
2018-02-14 00:18:09 +0000

    add documentation

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -1,6 +1,12 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+* free_grid - frees a 2 dimensional grid from memory
+* @height: number of rows
+* @grid: pointer to a pointer to 2d array
+* Return: void
+*/
 void free_grid(int **grid, int height)
 {
 	int i;
