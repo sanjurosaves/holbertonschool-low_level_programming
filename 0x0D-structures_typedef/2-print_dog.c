3d33a89e0fad615d0bf2084b66c068ba7db157f6
Alex Allen <325@holbertonschool.com>
2018-02-20 18:47:30 +0000

    not checking for null age

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -12,10 +12,9 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 	printf("Name: %s\n", ((d->name == NULL) ? "(nil)" : d->name));
-	if (d->age != 0.0)
-		printf("Age: %f\n", d->age);
-	else
-		printf("Age: (nil)\n");
+
+	printf("Age: %f\n", d->age);
+
 	printf("Owner: %s\n", ((d->owner == NULL) ? "(nil)" : d->owner));
 	}
 
