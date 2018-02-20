c82a5e6241586b7dc56e64d570490cdd048bd155
Alex Allen <325@holbertonschool.com>
2018-02-20 19:21:03 +0000

    check to see if name is null

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,13 +11,43 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *newdog;
+	int ownlen, namelen, i;
 
 	newdog = malloc(sizeof(struct dog));
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = name;
+
+	for (ownlen = 0; owner[ownlen] != '\0'; ownlen++)
+		;
+	for (namelen = 0; name[namelen] != '\0'; namelen++)
+		;
+
+	newdog->name = malloc((namelen + 1) * sizeof(newdog->name));
+	if (newdog->name == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
+
+	for (i = 0; i <= namelen; i++)
+		newdog->name[i] = name[i];
+
+	newdog->owner = malloc((ownlen + 1) * sizeof(newdog->owner));
+	if (newdog->owner == NULL)
+	{
+		free(newdog);
+		free(newdog->name);
+		return (NULL);
+	}
+
+	for (i = 0; i <= ownlen; i++)
+		newdog->owner[i] = owner[i];
+
 	newdog->age = age;
-	newdog->owner = owner;
+
+	free(newdog->owner);
+	free(newdog->name);
+	free(newdog);
 
 	return (newdog);
 }
