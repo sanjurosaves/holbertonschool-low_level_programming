4412b12484a9c226f3f5d3568876a1c02390d47a
Alex Allen <325@holbertonschool.com>
2018-02-20 18:40:49 +0000

    checking for null

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -10,9 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = NULL;
-	d->age = 0.0;
-	d->owner = NULL;
+	if (!d)
+		return;
 
 	d->name = name;
 	d->age = age;
