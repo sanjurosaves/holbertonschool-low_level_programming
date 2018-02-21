ec10b0d2a1681243216e84c9925a971cff372b73
Alex Allen <325@holbertonschool.com>
2018-02-21 21:12:58 +0000

    account for null values

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -11,6 +11,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if ((array == 0) | (size < 1) | (action == 0))
+		return;
+
 	for (i = 0; i < size; i++)
 		(*action)(array[i]);
 
