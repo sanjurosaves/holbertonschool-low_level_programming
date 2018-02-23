ce6980f8f8f65cb81819405209ed0c63df1e29af
Alex Allen <325@holbertonschool.com>
2018-02-23 22:58:56 +0000

    number 2 works

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -1,30 +1,37 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 
+/**
+ * print_strings - prints strings followed by a new line
+ * @n: number of parameters
+ * @separator: pointer to a char containing first char of separator string
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list items;
+	char *tmp;
 
 	va_start(items, n);
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		if ((separator != NULL) && (va_arg(items, char *) == NULL))
-			printf("(nil)%s", separator);
-		else if ((separator == NULL) && (va_arg(items, char *) == NULL))
-			printf("(nil)");
-		else if (separator != NULL)
-			printf("%s%s", va_arg(items, char *), separator);
-		else if (separator == NULL)
-			printf("%s", va_arg(items, char *));
+		tmp = va_arg(items, char *);
 
+		if (tmp == NULL)
+			printf("(nil)");
+		else
+			printf("%s", tmp);
+
+		if (separator != NULL)
+			printf("%s", separator);
 	}
 
-	if ((va_arg(items, char *) == NULL))
+	tmp = va_arg(items, char *);
+	if (tmp == NULL)
 		printf("(nil)\n");
 	else
-		printf("%s\n", va_arg(items, char *));
+		printf("%s\n", tmp);
 
 	va_end(items);
 }
