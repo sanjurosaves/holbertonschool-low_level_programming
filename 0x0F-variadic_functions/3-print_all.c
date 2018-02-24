ecf08a483a2463bf5fffcbf228c8f30db0871a2c
Alex Allen <325@holbertonschool.com>
2018-02-24 23:56:12 +0000

    account for NULL format

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -64,7 +64,7 @@ void print_all(const char * const format, ...)
 	char *sep = "";
 
 	va_start(valist, format);
-	while (format[i] != '\0')
+	while (format[i] != '\0' && format != NULL)
 	{
 		j = 0;
 		while (j < 4)
