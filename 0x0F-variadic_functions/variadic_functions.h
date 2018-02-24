70059cb74451e942188bef1fd3b1374ad13c2452
Alex Allen <325@holbertonschool.com>
2018-02-24 22:50:03 +0000

    update header file

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -2,12 +2,26 @@
 #define VARIADIC_H_
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct type - data type
+ *
+ * @a: single char abreviation
+ * @f: The function associated
+ */
+typedef struct type
+{
+	char *a;
+	void (*f)(va_list a);
+} data_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 
 #endif
