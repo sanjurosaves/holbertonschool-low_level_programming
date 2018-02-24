6008411ac29317c915c92265a19805c668562044
Alex Allen <325@holbertonschool.com>
2018-02-24 22:45:48 +0000

    add task 3

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,87 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_str - prints string
+ * @s: string to print
+ * Return: void
+ */
+void print_str(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "nil";
+	printf("%s", str);
+}
+
+/**
+ * print_float - prints float
+ * @d: double to print (all floats upgraded)
+ * Return: void
+ */
+void print_float(va_list d)
+{
+	printf("%f", va_arg(d, double));
+}
+
+/**
+ * print_int - prints integer
+ * @i: int to print
+ * Return: void
+ */
+void print_int(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_char - prints character
+ * @c: char to print
+ * Return: void;
+ */
+void print_char(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_all - prints anything
+ * @format: type of argument passed
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	data_t type[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+	unsigned int i = 0, j;
+	char *sep = "";
+
+	va_start(valist, format);
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (j < 4)
+		{
+/*			getchar(); printf("i = %d, j = %d\n", i, j);*/
+			if (format[i] == *type[j].a)
+			{
+				printf("%s", sep);
+				type[j].f(valist);
+				sep = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+
+	va_end(valist);
+}
