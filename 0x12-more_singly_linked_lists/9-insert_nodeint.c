336a0e07861610a807ed53b25dbda4cd6762d685
Alex Allen <325@holbertonschool.com>
2018-03-07 21:40:37 +0000

    handling index of zero

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ * add_nodeint - adds new node at beginning of linked list
+ * @n: integer value
+ * @head: pointer to pointer to list head
+ * Return: address of the new element, or, in the event of failure, NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+        listint_t *new;
+
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+                return (NULL);
+
+        new->n = n;
+
+        new->next = *head;
+
+        *head = new;
+
+        return (*head);
+}
+
+/**
  * insert_nodeint_at_index - inserts new node at a given position
  * @n: interger value
  * @idx: index of list where new node will be added
@@ -13,6 +36,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *tmphead;
 
 	tmphead = *head;
+
+       	if (idx == 0)
+		return (add_nodeint(head, n));
 
 	while ((*head != NULL) && (count <= idx))
 	{
