2bb491d8131b56c8325f6819bdba6a01fa47105e
Alex Allen <325@holbertonschool.com>
2018-03-07 22:31:02 +0000

    handling an index that is beyond the bounds of the list

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * listint_len - returns num of elements in a linked list
+ * @h: pointer to list head
+ * Return: number of elements in list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count + 1);
+}
+
+/**
  * delete_nodeint_at_index - deletes node from linked list at specified index
  * @index: index of node to be deleted
  * @head: pointer to pointer to list head
@@ -8,10 +29,12 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int count = 0;
-	listint_t *junk, *tmphead, *tmphead2;
+	unsigned int count = 0; listint_t *junk, *tmphead, *tmphead2;
 
 	if (*head == NULL)
+		return (-1);
+
+	if (index > listint_len(*head))
 		return (-1);
 
 	tmphead = *head;
@@ -27,9 +50,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 				junk = *head;
 				tmphead2->next = (*head)->next;
 				*head = (*head)->next;
-				free(junk);
-				*head = tmphead;
-				break;
+				free(junk); *head = tmphead; break;
 			}
 			else
 			{
@@ -42,9 +63,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	else
 	{
-				junk = *head;
-				*head = (*head)->next;
-				free(junk);
+		junk = *head;
+		*head = (*head)->next;
+		free(junk);
 	}
 
 	return (1);
