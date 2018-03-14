337efe4d9c2989ea0b323338ad2f48057e9dfe54
Alex Allen <325@holbertonschool.com>
2018-03-14 20:56:35 +0000

    check for text_content being null

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -16,15 +16,18 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC,
 		  S_IRUSR | S_IWUSR);
-
 	if (fd == -1)
 		return (-1);
 
-	for (len = 0; text_content[len] != '\0'; len++)
+	if (text_content == NULL)
 		;
+	else
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
 
-	write(fd, text_content, len);
-
+		write(fd, text_content, len);
+	}
 	close(fd);
 
 	return (1);
