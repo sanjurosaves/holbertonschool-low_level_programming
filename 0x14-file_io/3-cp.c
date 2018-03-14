6570dc7334c0669f7419d77bf78fb2c9e4ba83ce
Alex Allen <325@holbertonschool.com>
2018-03-14 23:00:01 +0000

    read write error handling

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,6 +1,30 @@
 #include "holberton.h"
 
 /**
+ * rw_errors - returns errors if read or write fails
+ * @c: error type indicator (r for read; w for write)
+ * @rw_return: return integer of read/write function
+ * @filename: name of file
+ */
+void rw_errors(int rw_return, char c, char *filename)
+{
+	if (rw_return != -1)
+		return;
+	else if (c == 'r')
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n",
+			filename);
+		exit(98);
+	}
+	else if (c == 'w')
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+			filename);
+		exit(99);
+	}
+}
+
+/**
  * main - emulation of linux cp command
  * @argv: argv[1] is source file to be copied, argv[2] is destination file
  * @argc: argument count
@@ -8,7 +32,8 @@
  */
 int main(int argc, char **argv)
 {
-	int rfd, wfd, rclose, wclose, remain, writ; char buffer[1024]; char *bp;
+	int rfd, wfd, rclose, wclose, remain, writ;
+	char c = '0', buffer[1024]; char *bp;
 
 	if (argc != 3)
 	{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"); exit(97); }
@@ -24,13 +49,13 @@ int main(int argc, char **argv)
 
 	while (1)
 	{
-		remain = read(rfd, buffer, 1024);
+		remain = read(rfd, buffer, 1024); c = 'r'; rw_errors(remain, c, argv[1]);
 		if (remain > 0)
 		{
 			bp = buffer;
 			while (remain > 0)
 			{
-				writ = write(wfd, bp, remain);
+				writ = write(wfd, bp, remain);  c = 'w'; rw_errors(remain, c, argv[2]);
 				remain -= writ;
 				bp += writ;
 			}
@@ -41,8 +66,7 @@ int main(int argc, char **argv)
 
 	rclose = close(rfd);
 	if (rclose == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't close %d\n", rfd); exit(100); }
-
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", rfd); exit(100); }
 	wclose = close(wfd);
 	if (wclose == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", wfd); exit(100); }
