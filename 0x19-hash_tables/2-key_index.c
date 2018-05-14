2c313343aa6a48f29ce9420c3cb4210a08b671e6
Alex Allen <325@holbertonschool.com>
2018-05-14 06:21:30 +0000

    add task 2

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index = returns index of a hash key
+ * @key: pointer to key string
+ * @size: size of hash table
+ * Return: index of hash key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index, hashkey;
+
+	hashkey = hash_djb2(key);
+	index = hashkey % size;
+
+	return (index);
+}
