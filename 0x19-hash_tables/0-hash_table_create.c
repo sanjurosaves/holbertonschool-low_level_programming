0ed4b7966252c088446ec66f8ceff781b0eee197
Alex Allen <325@holbertonschool.com>
2018-05-14 05:54:27 +0000

    document and fix return style

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,6 +1,11 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to newly created hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_node_t **array;
@@ -21,5 +26,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->size = size;
 	ht->array = array;
 
-	return ht;
+	return (ht);
 }
