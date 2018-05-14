b640520fef60aa1cc47ccc4023e88c080eb27700
Alex Allen <325@holbertonschool.com>
2018-05-14 05:51:47 +0000

    0-hash_table_create.c

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_node_t **array;
+	hash_table_t *ht;
+	unsigned long int i;
+
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = array;
+
+	return ht;
+}
