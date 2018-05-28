d47479db7651888b0c5082cc1814aead4044fff5
Alex Allen <325@holbertonschool.com>
2018-05-28 01:24:59 +0000

    betty and documentation fixes

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,13 +1,18 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - use bubble sort algorithm to sort array of ints
+ * @array: poiner to the array
+ * @size: length of array
+ * Return: void
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t n = size;
 	unsigned short swapped, i;
 	int tmp;
 
-	do
-	{
+	do {
 		swapped = 0;
 		for (i = 0; i < n; i++)
 		{
@@ -20,6 +25,5 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
-	}
-	while (swapped == 1);
+	} while (swapped == 1);
 }
