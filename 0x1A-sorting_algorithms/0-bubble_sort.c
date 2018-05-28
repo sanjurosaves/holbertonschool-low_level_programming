8ddb8f0ceacb0721e7cfc5a862979b47820e7efe
Alex Allen <325@holbertonschool.com>
2018-05-28 01:21:53 +0000

    add bubble sort task

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,36 +1,25 @@
 #include "sort.h"
-/**
- * swap - swap the values of two array indexes
- *
- * @xp: a pointer to the first array index
- * @yp: a pointer to the second array index
- *
- * Return: none
- */
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-/**
- * bubble_sort - use bubble sort algorhithm to sort array of integers
- *
- * @array: pointer to the array to be sorted
- * @size: length of the array
- *
- * Return: none/void
- */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t outer, inner;
+	size_t n = size;
+	unsigned short swapped, i;
+	int tmp;
 
-	for (outer = 0; outer < size - 1; outer++)
-		for (inner = 0; inner < size - outer - 1; inner++)
-			if (array[inner] > array[inner + 1])
+	do
+	{
+		swapped = 0;
+		for (i = 0; i < n; i++)
+		{
+			if (array[i] < array[i - 1])
 			{
-				swap(&array[inner], &array[inner + 1]);
+				tmp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = tmp;
+				swapped = 1;
 				print_array(array, size);
 			}
-
+		}
+	}
+	while (swapped == 1);
 }
