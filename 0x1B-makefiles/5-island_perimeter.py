19f5198e5f9885a80a746cad4585dead344304ea
Alex Allen <sanjurosaves@outlook.com>
2018-06-16 23:12:39 -0700

    added try/except to handle list out of bounds

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -21,8 +21,11 @@ def get_cell_perim_val(row, col, grid):
     cpv = 0
     coordinates = [(row, col-1), (row, col+1), (row-1, col), (row+1, col)]
 
-    for e in coordinates:
-        if (grid[e[0]][e[1]] == 0) or ((e[0] < 0) or (e[1] < 0)):
+    try:
+        for e in coordinates:
+            if (grid[e[0]][e[1]] == 0) or ((e[0] < 0) or (e[1] < 0)):
+                cpv += 1
+    except:
             cpv += 1
             
     return(cpv)
