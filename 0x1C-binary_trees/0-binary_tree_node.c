ca5ff151509da4e030ff981c74044fddf88430b3
Alex Allen <325@holbertonschool.com>
2018-07-03 22:42:23 +0000

    set children to null to prevent print/height function from segfaulting

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -18,6 +18,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		newnode->parent = parent;
 
 	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
 
 	return (newnode);
 }
