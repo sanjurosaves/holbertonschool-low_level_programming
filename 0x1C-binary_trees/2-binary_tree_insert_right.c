d0215d5689e9734f1e164698185a7be59ecd16c2
Alex Allen <325@holbertonschool.com>
2018-07-03 03:49:59 +0000

    add task 2

---
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode, *tmp;
+
+	if (parent == NULL)
+		return (NULL);
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+	if (parent->right == NULL)
+		parent->right = newnode;
+	else
+	{
+		tmp = parent->right;
+		parent->right = newnode;
+		newnode->right = tmp;
+		tmp->parent = newnode;
+	}
+
+	return (newnode);
+}
